858746e55375cbea3e890ff9e85cb660668a869d
MIVS <admin@appli-suivi-projets.com>
2026-08-17 19:10:09 +0000

    Add STATICFILES_DIRS to fix static files serving

--- askfordata/settings.py
+++ askfordata/settings.py
@@ -143,6 +143,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'api' / 'static']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
